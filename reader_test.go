134c57fd9564df3ec203a6be9f3c27915c20ad61
Michael Cross <mc@xi2.org>
2015-07-18 01:29:00 +0100

    Correct typo

--- reader_test.go
+++ reader_test.go
@@ -28,7 +28,7 @@ type testFile struct {
 	err    error
 }
 
-// Note that the md5sums below were generatad with XZ Utils and not
+// Note that the md5sums below were generated with XZ Utils and not
 // with this package.
 
 var badFiles = []testFile{
